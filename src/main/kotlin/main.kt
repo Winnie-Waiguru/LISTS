d3365f8b18b0ed594fc25d47456554fc9320d9e8
Winnie-Waiguru <winniewaiguru@gmail.com>
2022-04-15 20:39:26 +0300

    first commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,44 @@
-fun main (){
-    var y= evenIndicies(listOf("Winfred", "Beatrice", "Faith", "Gumato", "Jane", "Christine", "Esther", "Nelius", "Grace", "Juliet"))
+import jdk.jfr.Registered
+
+fun main () {
+    var y = evenIndicies(
+        listOf(
+            "Winfred",
+            "Beatrice",
+            "Faith",
+            "Gumato",
+            "Jane",
+            "Christine",
+            "Esther",
+            "Nelius",
+            "Grace",
+            "Juliet"
+        )
+    )
     println(y)
-    var x= averagesOfHeight(listOf(5.6, 3.0, 4.0, 6.7, 2.9))
+    var x = averagesOfHeight(listOf(5.6, 3.0, 4.0, 6.7, 2.9))
     println(x)
 
-    var a= Person("Winfred", 30, 5.9, 60)
-    var b= Person("Grace", 50, 4.3, 50)
-    var c=Person("Nelius", 40, 5.2, 45)
+    var a = Person("Winfred", 30, 5.9, 60)
+    var b = Person("Grace", 50, 4.3, 50)
+    var c = Person("Nelius", 40, 5.2, 45)
     var sorted = mutableListOf(a, b, c)
     println(sorted.sortedByDescending { n -> n.age })
 
+    var d= Person("Natasha", 35, 6.9, 60)
+    var e = Person("Lucia", 45, 4.3, 50)
+     var newlist= mutableListOf(d, e)
+    println(newlist.plus(sorted))
 
-    }
+
+    var benz= Car("KCN8790", 60.00)
+    var Prado= Car("KDA2345", 40.9)
+    var xy= listOf(benz, Prado)
+    println(xy)
+
+
+}
+
 
 
 fun evenIndicies(names:List<String>):List<String>{
@@ -35,10 +62,20 @@ fun averagesOfHeight(heights:List<Double>):String{
     return result
 }
 
-data class Person(var name:String, var age:Int, var height:Double, var weight:Int){
+data class Person(var name:String, var age:Int, var height:Double, var weight:Int) {
 
 }
 
-data class Person2(var name:String, var age:Int, var height:Double, var weight:Int){
+fun person(names2:List<Any>):List<Any>{
+    return listOf()
+}
 
+data class Car(var registration:String, var mileage:Double){
+     fun averageMilage(car:List<Car>):Double{
+         var total =0.0
+         car.forEach{c ->
+             total+=c.mileage
+         }
+         return total/car.count()
+     }
 }
